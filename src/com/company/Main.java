7ff9fba3e700325c6566754c1fa0d8fa52f3217f
taitmon <taitmonlynch@gmail.com>
2019-01-24 13:53:40 -0600

    check in

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,13 +5,24 @@ public class Main
 
     public static void main(String[] args)
     {
+
         String returnValue = getMyName();
         System.out.println(returnValue);
+
+        String myFancyName = getMyFancyName("#!#");
+        System.out.println(myFancyName);
     }
 
     private static String getMyName()
     {
         String name = "Taitmon";
         return name;
+    }
+
+    private static String getMyFancyName(String decoration)
+    {
+        String fancyName = decoration + "Taitmon" + decoration;
+        return fancyName;
+
     }
 }
