f12807041ba665c092f502d702c02062b19c9157
taitmon <taitmonlynch@gmail.com>
2019-01-24 09:02:41 -0600

    check in

---
+++ src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class Main
+{
+
+    public static void main(String[] args)
+    {
+        String returnValue = getMyName();
+        System.out.println(returnValue);
+    }
+
+    private static String getMyName()
+    {
+        String name = "Taitmon";
+        return name;
+    }
+}
